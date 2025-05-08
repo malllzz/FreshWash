a5dcd6624e638ebcc91fc57cf33cc26c7aa5d46d
malllzz <aziz.ramadhan1026@gmail.com>
2025-05-08 23:40:50 +0700

    move routing logic from App.jsx to router.jsx

--- freshwash-frontend/src/App.jsx
+++ freshwash-frontend/src/App.jsx
@@ -1,23 +1,7 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import Home from './pages/Home';
-import About from './pages/About';
-import Services from './pages/Services';
-import Reservation from './pages/Reservation';
+import Router from './routes/router'; // Mengimpor komponen Router dari file router.jsx
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/services" element={<Services />} />
-      <Route path="/reservation" element={<Reservation />} />
-    </Routes>
-  );
+  return <Router />; // Hanya merender komponen Router
 }
 
 export default App;
